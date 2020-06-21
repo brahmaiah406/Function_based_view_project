480fa32b9296a03c588681ba42a69efac88934ef
bunny944 <38912033+bunny944@users.noreply.github.com>
2020-06-21 10:34:40 +0530

    function_based_view_project

---
+++ api_basic/serializers.py
@@ -0,0 +1,9 @@
+frofrom rest_framework import serializers
+from .models import Article
+
+
+class ArticleSerializers(serializers.ModelSerializer):
+	class Meta:
+		model = Article
+		# fields = ['id', 'title', 'author', 'email']
+		fields = '__all__'
